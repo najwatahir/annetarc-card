70461a16eb97041957ec7003466926638f67eecb
Najwa Tahir <najwatahir186@gmail.com>
2024-07-13 19:39:21 +0800

    Update Blog.tsx

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -10,7 +10,7 @@ export default function Blog() {
     }, []);
     
     return (
-        <div className="{`bg-[#FEFAEE] mb-6 ${isActive ? 'page active' : 'page'} `}">
+        <div className={`bg-[#FEFAEE] mb-6 ${isActive ? 'page active' : 'page'}`}">
             <div className="text-center flex flex-col items-center justify-center min-h-screen">
                 <p className="pixelify md:text-2xl text-lg">Blog coming soon! :D</p>
             </div>
