12c5abdd17bef218f42236c424a49967da681515
Najwa Tahir <najwatahir186@gmail.com>
2024-07-13 19:38:33 +0800

    Update Blog.tsx

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Footer from './Footer';
 
 export default function Blog() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        setIsActive(true);
+    }, []);
+    
     return (
-        <div className="bg-[#FEFAEE] mb-6">
+        <div className="{`bg-[#FEFAEE] mb-6 ${isActive ? 'page active' : 'page'} `}">
             <div className="text-center flex flex-col items-center justify-center min-h-screen">
                 <p className="pixelify md:text-2xl text-lg">Blog coming soon! :D</p>
             </div>
